a7f82ffbd18289218bc6bc46a12153492de18081
Maksym Salov <maksimsalov8@gmail.com>
2022-06-07 00:53:04 +0300

    use datePicker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'MaterialProject';
+
+  date: any
+  ageControl: boolean = true;
+  dateNow = Date.now()
+  adult = 1000 * 60 * 60 * 24 * 360 * 18;
+  min = this.dateNow - this.adult;
+  minDate = new Date(this.min);
+
+
+  constructor(){
+    console.log(this.date)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule  } from '@angular/material/slider';
-import {MatMenuModule} from "@angular/material/menu";
-import {MatButtonModule} from "@angular/material/button";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatButtonModule } from "@angular/material/button";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatNativeDateModule } from "@angular/material/core";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -14,9 +18,15 @@ import {MatButtonModule} from "@angular/material/button";
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatSliderModule,
     MatMenuModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule
+    // MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
