2d92f0929459c25ff98bd2cd7cdfc24b82bec42a
Maksym Salov <maksimsalov8@gmail.com>
2022-06-10 16:48:49 +0300

    trying change validator of age

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {AfterViewInit, Component, OnInit, ViewChild,} from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import {AbstractControl, FormBuilder, FormControl, Validators} from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogComponent } from "./dialog/dialog.component";
 import { DialogListComponent } from "./dialog-list/dialog-list.component";
@@ -24,111 +24,105 @@ export interface DialogData {
 })
 export class AppComponent implements OnInit{
   // @ViewChild(CdkVirtualScrollViewport) scroller: CdkVirtualScrollViewport;
-  post: BehaviorSubject<number> = new BehaviorSubject<number>(1);
-  postCount = 1
-
+  // post: BehaviorSubject<number> = new BehaviorSubject<number>(1);
+  // postCount = 1
+  //
   ageControl: boolean = true;
   dateNow = Date.now()
   adult = 1000 * 60 * 60 * 24 * 360 * 18;
   max = this.dateNow - this.adult;
   maxDate = new Date(this.max);
-  form: any;
+  // form: any;
 
-  get formControls() {
-    return this.form.controls;
-  }
+  // get formControls() {
+  //   return this.form.controls;
+  // }
 
   constructor(private fb: FormBuilder, public dialog: MatDialog,
               private http: HttpClient, private scroll: ScrollDispatcher,
               private _formBuilder: FormBuilder){
-    this.form = this.fb.group({
-      date: ['date']
-    })
+    // this.form = this.fb.group({
+    //   date: ['date',, Validators.required]
+    // })
 
 
   }
 
 
 
-  OnDateChange(event: any){
-    if(event.getTime() > this.maxDate.getTime()){
-      this.ageControl = false;
-    }else {
-      this.ageControl = true;
-    }
-  }
+
 
 // first dialog
 
-  animal: string = '';
-  name: string = '';
-
-  openDialog(): void {
-    const dialogRef = this.dialog.open(DialogComponent, {
-      width: '250px',
-      data: {name: this.name, animal: this.animal},
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-      this.animal = result;
-    });
-  }
+  // animal: string = '';
+  // name: string = '';
+  //
+  // openDialog(): void {
+  //   const dialogRef = this.dialog.open(DialogComponent, {
+  //     width: '250px',
+  //     data: {name: this.name, animal: this.animal},
+  //   });
+  //
+  //   dialogRef.afterClosed().subscribe(result => {
+  //     console.log('The dialog was closed');
+  //     this.animal = result;
+  //   });
+  // }
 // second dialog
 
-  openDialogList() {
-    this.dialog.open(DialogListComponent, {
-      data: {
-        animal: 'panda',
-      },
-    });
-  }
-
-  // scroll bar
-
-  items = Array.from({length: 100}).map((n, i) => `Item #${i}`);
-
-  // blog comments
-
-  array:string[] = [];
-
-  private onWindowScroll(data: any) {
-    const scrollTop = data.getElementRef().nativeElement.scrollTop || 0;
-    // if (this.lastOffset() > scrollTop) {
-    //   console.log('111');
-    // } else if (scrollTop < 10) {
-    //   console.log('222');
-    // } else if (scrollTop > 100) {
-    //   console.log('333');
-    // }
-    if(scrollTop > 0){
-      this.postCount++;
-      this.post.next(this.postCount);
-    }
-    console.log('scrollTop' + scrollTop)
-    // console.log('postCount' + this.postCount)
-    // console.log(this.post.value)
-    // this.lastOffset() = scrollTop;
-  }
-
-  private lastOffset() {}
-
-  ngOnInit() {
-    this.http
-      .get(`https://jsonplaceholder.typicode.com/posts/${this.post.value}/comments`)
-      .pipe(
-        map((value: any) => {
-          value.map((el: any) => this.array = [...this.array, el.email])
-          this.scroll
-            .scrolled()
-            .subscribe((data) => {
-              this.onWindowScroll(data);
-            });
-        }),
-        tap((val) => console.log(this.array))
-      )
-      .subscribe();
-  }
+  // openDialogList() {
+  //   this.dialog.open(DialogListComponent, {
+  //     data: {
+  //       animal: 'panda',
+  //     },
+  //   });
+  // }
+  //
+  // // scroll bar
+  //
+  // items = Array.from({length: 100}).map((n, i) => `Item #${i}`);
+  //
+  // // blog comments
+  //
+  // array:string[] = [];
+  //
+  // private onWindowScroll(data: any) {
+  //   const scrollTop = data.getElementRef().nativeElement.scrollTop || 0;
+  //   // if (this.lastOffset() > scrollTop) {
+  //   //   console.log('111');
+  //   // } else if (scrollTop < 10) {
+  //   //   console.log('222');
+  //   // } else if (scrollTop > 100) {
+  //   //   console.log('333');
+  //   // }
+  //   if(scrollTop > 0){
+  //     this.postCount++;
+  //     this.post.next(this.postCount);
+  //   }
+  //   console.log('scrollTop' + scrollTop)
+  //   // console.log('postCount' + this.postCount)
+  //   // console.log(this.post.value)
+  //   // this.lastOffset() = scrollTop;
+  // }
+  //
+  // private lastOffset() {}
+  //
+  // ngOnInit() {
+  //   this.http
+  //     .get(`https://jsonplaceholder.typicode.com/posts/${this.post.value}/comments`)
+  //     .pipe(
+  //       map((value: any) => {
+  //         value.map((el: any) => this.array = [...this.array, el.email])
+  //         this.scroll
+  //           .scrolled()
+  //           .subscribe((data) => {
+  //             this.onWindowScroll(data);
+  //           });
+  //       }),
+  //       tap((val) => console.log(this.array))
+  //     )
+  //     .subscribe();
+  // }
 
 
   // stepper
@@ -139,6 +133,31 @@ export class AppComponent implements OnInit{
   secondFormGroup = this._formBuilder.group({
     secondCtrl: ['', [Validators.required, Validators.minLength(6)]],
   });
+  form = this._formBuilder.group(
+    {
+    date: ['', this.ValidateAge]
+    },
+    {
+
+    });
   isLinear = false;
 
+  OnDateChange(event: any){
+    if(event.getTime() > this.maxDate.getTime()){
+      this.ageControl = false;
+    }else {
+      this.ageControl = true;
+    }
+  }
+
+
+  ValidateAge(control: FormControl){
+    // const controlDate = new Date(control)
+    if(control.value){
+      console.log(control.value)
+    }
+  }
+
+  ngOnInit() {
+  }
 }
